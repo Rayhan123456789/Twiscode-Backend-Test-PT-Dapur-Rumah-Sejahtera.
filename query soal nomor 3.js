f943f2eaaf6c94288d151808027139807f914a02
Rayhan123456789 <87824990+Rayhan123456789@users.noreply.github.com>
2022-01-04 13:34:11 +0700

    Update query soal nomor 3.js

--- query soal nomor 3.js
+++ query soal nomor 3.js
@@ -1,13 +1,13 @@
 // nomor 1
-// SELECT * FROM public."Data Transaksi2"
-// ORDER BY "ID" ASC 
+SELECT * FROM public."Data Transaksi2"
+ORDER BY "ID" ASC 
 
 // nomor 2
-// SELECT * FROM public."Detail Transaksi"
-// ORDER BY "ID" ASC 
+SELECT * FROM public."Detail Transaksi"
+ORDER BY "ID" ASC 
 
 // nomor 3 
-// SELECT * FROM public."Transaksi jumlah detail"
-// ORDER BY id ASC 
+SELECT * FROM public."Transaksi jumlah detail"
+ORDER BY id ASC 
 
 // tinggal di tes di PgAdmin
